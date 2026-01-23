2db409bc5fac5083467801c5d7ad33e5eb7e9dc0
paulmaker <maker746@gmail.com>
2026-01-23 21:03:09 +0000

    asset form fix

--- office_mgmt/components/assets/asset-form.tsx
+++ office_mgmt/components/assets/asset-form.tsx
@@ -2,7 +2,6 @@
 
 import { useState } from 'react'
 import { useForm } from 'react-hook-form'
-import type { AssetType } from '@prisma/client'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
